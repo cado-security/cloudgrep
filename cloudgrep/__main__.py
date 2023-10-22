b9384eed03dc99c117973ec0dd9d85d0da7e7d2c
Chris Doman <cdoman@cadosecurity.com>
2023-10-22 18:22:11 +0100

    Reformat

--- cloudgrep/__main__.py
+++ cloudgrep/__main__.py
@@ -64,7 +64,7 @@ def main() -> None:
     )
     args = vars(parser.parse_args())
 
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         sys.exit(1)
 
