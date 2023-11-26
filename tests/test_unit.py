05bc83feaa01d15fbf30655b625349695f4fdda4
Chris Doman <cdoman@cadosecurity.com>
2023-11-26 23:26:40 +0000

    fix caps

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -12,7 +12,7 @@ from datetime import datetime
 
 from cloudgrep.cloud import Cloud
 from cloudgrep.search import Search
-from cloudgrep.cloudgrep import Cloudgrep
+from cloudgrep.cloudgrep import CloudGrep
 
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -71,7 +71,7 @@ class CloudGrepTests(unittest.TestCase):
         file = "queries.txt"
         with open(file, "w") as f:
             f.write(f"query1\n{_QUERY}\nquery3")
-        multi_query = Cloudgrep().load_queries(file)
+        multi_query = CloudGrep().load_queries(file)
         hits = Cloud().download_from_s3_multithread(_BUCKET, matching_keys, multi_query, False)
 
         # Upload a log 10 000 times and see how long it takes
@@ -112,6 +112,6 @@ class CloudGrepTests(unittest.TestCase):
         file = "queries.txt"
         with open(file, "w") as f:
             f.write("query1\nquery2\nquery3")
-        queries = Cloudgrep().load_queries(file)
+        queries = CloudGrep().load_queries(file)
         self.assertIsInstance(queries, str)
         self.assertEqual(queries, "query1|query2|query3")
